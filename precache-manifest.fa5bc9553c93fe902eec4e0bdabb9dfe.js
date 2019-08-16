cf563da1f47319b63a4a245142ce118664a2238b
Anna Krikunova <anna.krikunova99@gmail.com>
2019-08-16 12:47:33 +0300

    Updates

---
+++ precache-manifest.fa5bc9553c93fe902eec4e0bdabb9dfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b4b6e814ff983a0203731663aca85b9",
+    "url": "/calendar/static/media/Montserrat-Regular.9b4b6e81.woff"
+  },
+  {
+    "revision": "9bfcc487a6dfafce80ad2fba277f6ee2",
+    "url": "/calendar/static/media/Montserrat-ExtraBold.9bfcc487.woff"
+  },
+  {
+    "revision": "fe7c3f2645519a5fa02efa33267cc977",
+    "url": "/calendar/static/media/Montserrat-Bold.fe7c3f26.woff"
+  },
+  {
+    "revision": "70cd18d5f6522f88cc6f",
+    "url": "/calendar/static/js/runtime~main.3b46a14a.js"
+  },
+  {
+    "revision": "4f5c60baa3f7b0948a87",
+    "url": "/calendar/static/js/main.fea4e955.chunk.js"
+  },
+  {
+    "revision": "38a16849ea21588c04b8",
+    "url": "/calendar/static/js/2.99923879.chunk.js"
+  },
+  {
+    "revision": "4f5c60baa3f7b0948a87",
+    "url": "/calendar/static/css/main.0f3cbd26.chunk.css"
+  },
+  {
+    "revision": "08fbeca945b05d916884cd052929a663",
+    "url": "/calendar/index.html"
+  }
+];
